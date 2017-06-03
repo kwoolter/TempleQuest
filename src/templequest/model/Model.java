9c098ed9b730e8a829fde366bbed7330258a3351
kwoolter <keith.woolterton@gmail.com>
2017-06-03 15:24:28 +0800

    initial commit

---
+++ src/templequest/model/Model.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package templequest.model;
+
+/**
+ *
+ * @author user
+ */
+public class Model {
+    
+    private String sName;
+    
+    public Model(String sName) {
+        
+        this.sName = sName;
+        
+    }
+    
+}
