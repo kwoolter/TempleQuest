fce712aadc13756a2c8c9dfe5d7a4fea672d030e
kwoolter <keith.woolterton@gmail.com>
2017-06-03 15:00:01 +0800

    initial commit

--- src/templequest/TempleQuest.java
+++ src/templequest/TempleQuest.java
@@ -16,6 +16,9 @@ public class TempleQuest {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("\nHello World");
+        
     }
     
 }
